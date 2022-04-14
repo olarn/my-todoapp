cacc13f1015f1ebf2a69a307af9a337bca8e4cc1
Olarn U <olarn.u@odds.team>
2022-04-14 13:04:04 +0700

    Prepare to test Jenkins auto-build.

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ app.put('/items/:id', updateItem);
 app.delete('/items/:id', deleteItem);
 
 db.init().then(() => {
+    console.log("Server is starting...")
     app.listen(3000, () => console.log('Listening on port 3000'));
 }).catch((err) => {
     console.error(err);
